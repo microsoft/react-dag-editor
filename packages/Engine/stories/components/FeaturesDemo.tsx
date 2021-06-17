11cd94e8fdbe36e7679d49eb216f5e61ab1c3657
AMLStudio Dev <amlstudiodev@AMLStudios-MacBook-Pro.local>
2021-06-17 19:42:38 +0800

    update

--- packages/Engine/stories/components/FeaturesDemo.tsx
+++ packages/Engine/stories/components/FeaturesDemo.tsx
@@ -1,11 +1,43 @@
 import * as React from "react";
-import { Engine, Graph, GraphStateStore, GraphModel } from "../../src";
+import {
+  Engine,
+  Graph,
+  GraphStateStore,
+  GraphModel,
+  RegisterNode,
+  IRectConfig,
+  NodeModel,
+  getRectHeight,
+  getRectWidth
+} from "../../src";
 import { sampleGraphData } from "../data/sample-graph-1";
+
+const sourceNodeConfig: IRectConfig<NodeModel> = {
+  getMinHeight: () => 60,
+  getMinWidth: () => 100,
+  render(args): React.ReactNode {
+    const height = getRectHeight(sourceNodeConfig, args.model);
+    const width = getRectWidth(sourceNodeConfig, args.model);
+
+    return (
+      <ellipse
+        rx={width / 2}
+        ry={height / 2}
+        cx={args.model.x + width / 2}
+        cy={args.model.y + height / 2}
+        stroke="blue"
+        fill="blue"
+        fillOpacity={0.8}
+      />
+    );
+  }
+};
 
 export const FeaturesDemo: React.FC = () => {
   return (
-    <Engine>
+    <Engine style={{ width: "900px", height: "600px" }}>
       <GraphStateStore data={GraphModel.fromJSON(sampleGraphData)}>
+        <RegisterNode name="source" config={sourceNodeConfig} />
         <Graph />
       </GraphStateStore>
     </Engine>
--- packages/Engine/stories/data/sample-graph-1.ts
+++ packages/Engine/stories/data/sample-graph-1.ts
@@ -1,6 +1,16 @@
-import { ICanvasData } from '../../src';
+import { ICanvasData } from "../../src";
 
 export const sampleGraphData: ICanvasData = {
-  nodes: [],
+  nodes: [
+    {
+      id: "source",
+      name: "source",
+      ports: [],
+      data: {},
+      x: 100,
+      y: 100,
+      shape: "source"
+    }
+  ],
   edges: []
 };

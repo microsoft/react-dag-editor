003470e7a5497017bc7cc855f505c6da030dea5e
Wenlu Wang <wenlwan@microsoft.com>
2021-06-25 14:18:38 +0800

    Simplify build script

--- scripts/build-pages.js
+++ scripts/build-pages.js
@@ -1,8 +1,7 @@
 const path = require("path");
 const execa = require("execa");
-const glob = require("glob").sync;
 const fs = require('fs').promises;
-const fsSync = require('fs');
+const fse = require('fs-extra');
 
 const enginePath = path.resolve(__dirname, "../packages/ReactDagEditor");
 const demoSrc = path.join(enginePath, '.static-demo');
@@ -10,32 +9,26 @@ const demoDist = path.resolve(__dirname, '../docs');
 const gitIgnorePath = path.resolve(__dirname, '../.gitignore');
 
 async function buildDemo() {
-  console.log('Build start');
+  console.log('Build started.');
   await execa(`yarn --cwd ${enginePath} build:demo`, [], { stdio: "inherit", shell: true });
-  console.log('Build done');
+  console.log('Build done.');
 }
 
 async function copyDocs() {
-  console.log('Copy start');
-  if (!fsSync.existsSync(demoDist)) {
-    await fs.mkdir(demoDist);
-  }
-
-  const files = glob(`${demoSrc}/*`);
-  for (const file of files) {
-    await fs.copyFile(file, path.join(demoDist, path.basename(file)));
-  }
-  console.log('Copy done');
+  console.log('Copy started.');
+  await fse.ensureDir(demoDist);
+  await fse.copy(demoSrc, demoDist);
+  console.log('Copy done.');
 }
 
 async function rewriteGitIgnore() {
-  console.log('Rewrite gitignore start');
+  console.log('Rewrite gitignore started.');
   const gitIgnore = await fs.readFile(gitIgnorePath);
   const gitIgnoreContent = gitIgnore.toString();
   const lines = gitIgnoreContent.split(/\r?\n/);
   const newGitIgnoreContent = lines.filter(x => x.trim() !== 'docs/').join('\n');
   await fs.writeFile(gitIgnorePath, newGitIgnoreContent)
-  console.log('Rewrite gitignore done');
+  console.log('Rewrite gitignore done.');
 }
 
 async function main () {
@@ -44,4 +37,6 @@ async function main () {
   await rewriteGitIgnore();
 }
 
-main();
+main().then(() => {
+  console.log('All done.');
+});

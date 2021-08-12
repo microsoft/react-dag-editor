ba85199044c9f38a010f7b6cfb35407c41e50319
intellild <intellild@outlook.com>
2021-08-12 13:42:10 +0800

    delete PanelContext

--- packages/ReactDagEditor/src/contexts/index.ts
+++ packages/ReactDagEditor/src/contexts/index.ts
@@ -1,6 +1,5 @@
 export * from "./GraphConfigContext";
 export * from "./ContextMenuConfigContext";
-export * from "./PanelContext";
 export * from "./ThemeContext";
 export * from "./ViewportContext";
 export * from "./GraphStateContext";

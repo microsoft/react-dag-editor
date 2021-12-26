ba91c5299b2ba7cdd2541067d93016c668390186
intellild <intellild@outlook.com>
2021-12-26 16:00:02 +0800

    edges map mutator

--- packages/react-dag-editor/src/lib/content-utils/edge.ts
+++ packages/react-dag-editor/src/lib/content-utils/edge.ts
@@ -1,4 +1,9 @@
-import { IContentStateComputed } from "react-dag-editor";
+import {
+  IContentStateComputed,
+  IContentStateUpdate,
+} from "../models/ContentState";
+import { EdgeModel } from "../models/edge";
+import { EdgesMapMutator } from "./edges-map-mutator";
 
 export const getEdgesBySource =
   (
@@ -38,4 +43,39 @@ export const isEdgeExist =
       }
     });
     return exist;
+  };
+
+export const insertEdges =
+  (edges: EdgeModel[]): IContentStateUpdate =>
+  (content) => {
+    const nextNodes = content.nodes.mutate();
+    const nextEdges = content.edges.mutate();
+    const edgesBySource = new EdgesMapMutator(content.edgesBySource);
+    const edgesByTarget = new EdgesMapMutator(content.edgesByTarget);
+    edges.forEach((edge) => {
+      if (
+        isEdgeExist(
+          edge.source,
+          edge.sourcePortId,
+          edge.target,
+          edge.targetPortId
+        ) ||
+        !content.nodes.has(edge.source) ||
+        !content.nodes.has(edge.target)
+      ) {
+        return;
+      }
+      edgesBySource.addEdge(edge.id, edge.source, edge.sourcePortId);
+      edgesByTarget.addEdge(edge.id, edge.target, edge.targetPortId);
+      nextNodes
+        .update(edge.source, (node) => node.invalidCache())
+        .update(edge.target, (node) => node.invalidCache());
+      nextEdges.set(edge.id, edge);
+    });
+    return {
+      nodes: nextNodes.finish(),
+      edges: nextEdges.finish(),
+      edgesBySource: edgesBySource.finish(),
+      edgesByTarget: edgesByTarget.finish(),
+    };
   };
---
+++ packages/react-dag-editor/src/lib/content-utils/edges-map-mutator.ts
@@ -0,0 +1,50 @@
+import { EdgesByPort } from "react-dag-editor";
+import { HashMap, HashMapBuilder } from "../collections";
+
+export class EdgesMapMutator {
+  private result: HashMapBuilder<string, Map<string, Set<string>>>;
+  private source: HashMap<string, ReadonlyMap<string, ReadonlySet<string>>>;
+
+  constructor(edgesByPort: EdgesByPort) {
+    this.result = HashMap.empty<string, Map<string, Set<string>>>().mutate();
+    this.source = edgesByPort;
+  }
+
+  public addEdge(
+    edgeId: string,
+    nodeId: string,
+    portId: string
+  ): EdgesMapMutator {
+    const edges = this.getEdgesByPort(nodeId, portId);
+    edges.add(edgeId);
+    return this;
+  }
+
+  public finish(): HashMap<string, ReadonlyMap<string, ReadonlySet<string>>> {
+    this.source.forEach((ports, nodeId) => {
+      ports.forEach((prevEdges, portId) => {
+        const edges = this.getEdgesByPort(nodeId, portId);
+        prevEdges.forEach((edgeId) => {
+          edges.add(edgeId);
+        });
+      });
+    });
+    const result = this.result.finish();
+    this.result = HashMap.empty<string, Map<string, Set<string>>>().mutate();
+    return result;
+  }
+
+  private getEdgesByPort(nodeId: string, portId: string): Set<string> {
+    let node = this.result.get(nodeId);
+    if (!node) {
+      node = new Map();
+      this.result.set(nodeId, node);
+    }
+    let port = node.get(portId);
+    if (!port) {
+      port = new Set();
+      node.set(portId, port);
+    }
+    return port;
+  }
+}

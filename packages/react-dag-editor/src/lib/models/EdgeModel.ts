e3cfff12850465f56b872e97233ecf090d0d9790
intellild <intellild@outlook.com>
2022-01-03 16:15:30 +0800

    migrate models to macro based

--- packages/react-dag-editor/src/lib/models/EdgeModel.ts
+++ packages/react-dag-editor/src/lib/models/EdgeModel.ts
@@ -1,76 +1,33 @@
+import { RecordBase } from "record-class";
 import { $Complete } from "../utils/complete";
-import { preventSpread } from "../utils/preventSpread";
 import { ICanvasEdge } from "./edge";
-import { GraphEdgeStatus, updateStatus } from "./status";
+import { GraphEdgeStatus } from "./status";
 
-export class EdgeModel<T = unknown> implements $Complete<ICanvasEdge> {
-  public readonly inner: ICanvasEdge<T>;
-
-  public get id(): string {
-    return this.inner.id;
-  }
-
-  public get shape(): string | undefined {
-    return this.inner.shape;
-  }
-
-  public get automationId(): string | undefined {
-    return this.inner.automationId;
-  }
-
-  public get source(): string {
-    return this.inner.source;
-  }
-
-  public get target(): string {
-    return this.inner.target;
-  }
-
-  public get sourcePortId(): string {
-    return this.inner.sourcePortId;
-  }
-
-  public get targetPortId(): string {
-    return this.inner.targetPortId;
-  }
-
-  public get status(): GraphEdgeStatus | undefined {
-    return this.inner.status;
-  }
-
-  public get data(): Readonly<T> | undefined {
-    return this.inner.data;
-  }
-
-  private constructor(edge: ICanvasEdge<T>) {
-    this.inner = edge;
-    preventSpread(this);
-  }
-
-  public static fromJSON<D>(inner: ICanvasEdge<D>): EdgeModel<D> {
-    return new EdgeModel(inner);
-  }
-
-  public updateStatus(f: (state: number | undefined) => number): EdgeModel<T> {
-    return this.update(updateStatus(f));
-  }
-
-  public update(f: (curEdge: ICanvasEdge<T>) => ICanvasEdge<T>): EdgeModel<T> {
-    const edge = f(this.inner);
-    return edge === this.inner ? this : new EdgeModel(edge);
-  }
-
+export class EdgeModel<T = unknown>
+  extends RecordBase<ICanvasEdge<T>, EdgeModel<T>>
+  implements $Complete<ICanvasEdge<T>>
+{
+  public readonly id!: string;
   /**
-   * shallow copy the model
-   * example usage: mark the edge is dirty and need a re-render
-   *
-   * @returns shallowed edge model
+   * source node id
    */
-  public shallow(): EdgeModel<T> {
-    return new EdgeModel(this.inner);
+  public readonly source!: string;
+  /**
+   * target node id
+   */
+  public readonly target!: string;
+  public readonly sourcePortId!: string;
+  public readonly targetPortId!: string;
+  public readonly shape: string | undefined = undefined;
+  public readonly status: GraphEdgeStatus | undefined = undefined;
+  public readonly automationId: string | undefined = undefined;
+  public readonly data: Readonly<T> | undefined = undefined;
+
+  public static fromJSON<T = unknown>(source: ICanvasEdge<T>): EdgeModel<T> {
+    return new EdgeModel(source);
   }
 
-  public toJSON(): ICanvasEdge<T> {
-    return this.inner;
+  protected $$create(partial: ICanvasEdge<T>): EdgeModel<T> {
+    return new EdgeModel(partial);
   }
 }
--- packages/react-dag-editor/src/lib/models/NodeModel.ts
+++ packages/react-dag-editor/src/lib/models/NodeModel.ts
@@ -1,127 +1,59 @@
+import { RecordBase } from "record-class";
 import { mapCow } from "../utils/array";
 import type { $Complete } from "../utils/complete";
 import { getPortPositionByPortId } from "../utils/getPortPosition";
-import { preventSpread } from "../utils/preventSpread";
 import type { IGraphConfig } from "./config/types";
 import type { IPoint } from "./geometry";
 import type { ICanvasNode } from "./node";
 import type { ICanvasPort } from "./port";
-import { GraphNodeStatus, updateStatus } from "./status";
+import { GraphNodeStatus } from "./status";
+
+export interface INodeModel<NodeData = unknown, PortData = unknown>
+  extends ICanvasNode<NodeData, PortData> {
+  readonly portPositionCache?: Map<string, IPoint>;
+  readonly prev?: string;
+  readonly next?: string;
+}
 
 export class NodeModel<NodeData = unknown, PortData = unknown>
-  implements $Complete<ICanvasNode<NodeData, PortData>>
+  extends RecordBase<
+    INodeModel<NodeData, PortData>,
+    NodeModel<NodeData, PortData>
+  >
+  implements $Complete<INodeModel<NodeData, PortData>>
 {
-  public readonly inner: ICanvasNode<NodeData, PortData>;
-  public readonly portPositionCache: Map<string, IPoint | undefined>;
-  public readonly prev: string | undefined;
-  public readonly next: string | undefined;
+  public readonly id!: string;
+  public readonly x!: number;
+  public readonly y!: number;
+  public readonly name!: string;
+  public readonly height: number | undefined = undefined;
+  public readonly width: number | undefined = undefined;
+  public readonly shape: string | undefined = undefined;
+  public readonly status: GraphNodeStatus | undefined = undefined;
+  public readonly automationId: string | undefined = undefined;
+  public readonly ports: ReadonlyArray<ICanvasPort<PortData>> | undefined =
+    undefined;
+  public readonly portPositionCache = new Map<string, IPoint>();
+  public readonly data: Readonly<NodeData> | undefined = undefined;
+  public readonly ariaLabel: string | undefined = undefined;
+  public readonly prev: string | undefined = undefined;
+  public readonly next: string | undefined = undefined;
 
-  public get id(): string {
-    return this.inner.id;
-  }
-
-  public get status(): GraphNodeStatus | undefined {
-    return this.inner.status;
-  }
-
-  public get shape(): string | undefined {
-    return this.inner.shape;
-  }
-
-  public get ports(): ReadonlyArray<ICanvasPort<PortData>> | undefined {
-    return this.inner.ports;
-  }
-
-  public get ariaLabel(): string | undefined {
-    return this.inner.ariaLabel;
-  }
-
-  public get name(): string | undefined {
-    return this.inner.name;
-  }
-
-  public get x(): number {
-    return this.inner.x;
-  }
-
-  public get y(): number {
-    return this.inner.y;
-  }
-
-  public get automationId(): string | undefined {
-    return this.inner.automationId;
-  }
-
-  public get data(): Readonly<NodeData> | undefined {
-    return this.inner.data;
-  }
-
-  public get height(): number | undefined {
-    return this.inner.height;
-  }
-
-  public get width(): number | undefined {
-    return this.inner.width;
-  }
-
-  private constructor(
-    node: ICanvasNode<NodeData, PortData>,
-    portPositionCache: Map<string, IPoint | undefined>,
-    prev: string | undefined,
-    next: string | undefined
-  ) {
-    this.inner = node;
-    this.portPositionCache = portPositionCache;
-    this.prev = prev;
-    this.next = next;
-    preventSpread(this);
-  }
-
-  public static fromJSON<N, P>(
-    node: ICanvasNode<N, P>,
-    prev: string | undefined,
-    next: string | undefined
-  ): NodeModel<N, P> {
-    return new NodeModel(node, new Map(), prev, next);
+  public static fromJSON<NodeData = unknown, PortData = unknown>(
+    source: ICanvasNode<NodeData, PortData>,
+    prev: string | undefined = undefined,
+    next: string | undefined = undefined
+  ): NodeModel<NodeData, PortData> {
+    return new NodeModel({
+      ...source,
+      prev,
+      next,
+      portPositionCache: new Map(),
+    });
   }
 
   public getPort(id: string): ICanvasPort<PortData> | undefined {
     return this.ports?.find((port) => port.id === id);
-  }
-
-  public link({
-    prev,
-    next,
-  }: {
-    prev?: string | undefined;
-    next?: string | undefined;
-  }): NodeModel<NodeData, PortData> {
-    if (prev === this.prev && next === this.next) {
-      return this;
-    }
-    return new NodeModel(
-      this.inner,
-      this.portPositionCache,
-      prev ?? this.prev,
-      next ?? this.next
-    );
-  }
-
-  public updateStatus(
-    f: (state: number | undefined) => number
-  ): NodeModel<NodeData, PortData> {
-    return this.update(updateStatus(f));
-  }
-
-  public update(
-    f: (
-      curNode: ICanvasNode<NodeData, PortData>
-    ) => ICanvasNode<NodeData, PortData>
-  ): NodeModel<NodeData, PortData> {
-    const node = f(this.inner);
-    return node === this.inner
-      ? this
-      : new NodeModel(node, new Map(), this.prev, this.next);
   }
 
   public updateData(
@@ -130,16 +62,12 @@ export class NodeModel<NodeData = unknown, PortData = unknown>
     if (!this.data) {
       return this;
     }
-    return this.update((inner) => {
-      const data = f(inner.data as Readonly<NodeData>);
-      if (data === inner.data) {
-        return inner;
-      }
-      return {
-        ...inner,
-        data,
-      };
-    });
+    const data = f(this.data);
+    return this.data === data
+      ? this
+      : this.merge({
+          data,
+        });
   }
 
   public getPortPosition(
@@ -148,14 +76,16 @@ export class NodeModel<NodeData = unknown, PortData = unknown>
   ): IPoint | undefined {
     let point = this.portPositionCache.get(portId);
     if (!point) {
-      point = getPortPositionByPortId(this.inner, portId, graphConfig);
-      this.portPositionCache.set(portId, point);
+      point = getPortPositionByPortId(this, portId, graphConfig);
+      if (point) {
+        this.portPositionCache.set(portId, point);
+      }
     }
     return point;
   }
 
   public hasPort(id: string): boolean {
-    return Boolean(this.inner.ports?.find((port) => port.id === id));
+    return Boolean(this.ports?.find((port) => port.id === id));
   }
 
   /**
@@ -165,40 +95,38 @@ export class NodeModel<NodeData = unknown, PortData = unknown>
     dummy: ICanvasNode
   ): NodeModel<NodeData, PortData> {
     const { x, y, width, height } = dummy;
-    const node = {
-      ...this.inner,
+    return this.merge({
       x,
       y,
-      width: width ?? this.inner.width,
-      height: height ?? this.inner.height,
-    };
-    return new NodeModel(node, new Map(), this.prev, this.next);
+      width: width ?? this.width,
+      height: height ?? this.height,
+    });
   }
 
   public updatePorts(
     f: (port: ICanvasPort<PortData>, index: number) => ICanvasPort<PortData>
   ): NodeModel<NodeData, PortData> {
-    if (!this.inner.ports) {
+    if (!this.ports) {
       return this;
     }
-    const ports = mapCow(this.inner.ports, f);
-    const node =
-      this.inner.ports === ports
-        ? this.inner
-        : {
-            ...this.inner,
-            ports,
-          };
-    return node === this.inner
+    const ports = mapCow(this.ports, f);
+
+    return this.ports === ports
       ? this
-      : new NodeModel(node, new Map(), this.prev, this.next);
+      : this.merge({
+          ports,
+        });
   }
 
   public invalidCache(): NodeModel<NodeData, PortData> {
-    return new NodeModel(this.inner, new Map(), this.prev, this.next);
+    return this.merge({
+      portPositionCache: new Map(),
+    });
   }
 
-  public toJSON(): ICanvasNode<NodeData, PortData> {
-    return this.inner;
+  protected override $$create(
+    partial: INodeModel<NodeData, PortData>
+  ): NodeModel<NodeData, PortData> {
+    return new NodeModel(partial);
   }
 }

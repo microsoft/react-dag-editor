474f5799cc4286b8a3143db40b3aef34bdaab12c
intellild <intellild@outlook.com>
2022-01-04 15:09:58 +0800

    spread warning

--- packages/record-class/src/lib/record.ts
+++ packages/record-class/src/lib/record.ts
@@ -2,13 +2,32 @@ export interface IRecordApplicable<T, R> {
   (value: T): R;
 }
 
+const DONT_SPREAD_CLASS_INSTANCE = "DONT_SPREAD_CLASS_INSTANCE";
+
+const __DEV__ = process.env.NODE_ENV !== "production";
+
 export abstract class RecordBase<Interface, Class extends Interface> {
   public declare pipe: (
     ...list: ((value: Class) => Partial<Interface>)[]
   ) => Class;
   public declare merge: (partial: Partial<Interface>) => Class;
 
-  constructor(partial: Interface) {}
+  constructor(partial: Interface) {
+    if (__DEV__) {
+      Object.defineProperty(this, DONT_SPREAD_CLASS_INSTANCE, {
+        get() {
+          console.warn(
+            `${this.constructor.name} is a class, don't use spread syntax on its instance`
+          );
+        },
+      });
+      if (typeof this.pipe !== "function" || typeof this.merge !== "function") {
+        throw new Error(
+          "Missing methods 'pipe' and 'merge', did you forget to add the 'record' macro"
+        );
+      }
+    }
+  }
 
   public clone(): Class {
     return this.merge({});

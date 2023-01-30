7ddb3d2825284231f27899e0eb4b64e44406cacf
John Chou <luckyjoou@gmail.com>
2023-01-30 14:45:24 +0800

    Add key property to node layers

--- packages/react-dag-editor/src/lib/components/NodeLayers.tsx
+++ packages/react-dag-editor/src/lib/components/NodeLayers.tsx
@@ -4,7 +4,7 @@ import { OrderedMap } from "../collections";
 import { NodeModel } from "../models/NodeModel";
 interface INodeLayersProps {
   data: GraphModel;
-  renderTree(tree: OrderedMap<string, NodeModel>): JSX.Element;
+  renderTree(tree: OrderedMap<string, NodeModel>, layer: number): JSX.Element;
 }
 export const NodeLayers: React.FC<INodeLayersProps> = ({
   data,
@@ -17,7 +17,12 @@ export const NodeLayers: React.FC<INodeLayersProps> = ({
     <>
       {Array.from(layers.values())
         .sort()
-        .map((l) => renderTree(data.nodes.filter((n) => n.layer === l)))}
+        .map((l) =>
+          renderTree(
+            data.nodes.filter((n) => n.layer === l),
+            l
+          )
+        )}
     </>
   );
 };

20ef945e2100b45ee2db9b38884c15c83c3389ad
Jin Yucong <jinyucong911@outlook.com>
2023-02-16 13:37:24 +0000

    init

--- packages/react-dag-editor/src/lib/components/AnimatingNodeGroup.tsx
+++ packages/react-dag-editor/src/lib/components/AnimatingNodeGroup.tsx
@@ -11,55 +11,56 @@ interface IAnimatingNodeGroup {
   graphData: GraphModel;
 }
 
-export const AnimatingNodeGroup: React.FunctionComponent<IAnimatingNodeGroup> =
-  (props) => {
-    const { dummyNodes, graphData } = props;
-    const graphConfig = useGraphConfig();
-    const { dWidth, dHeight } = dummyNodes;
-    const dx = dummyNodes.alignedDX ?? dummyNodes.dx;
-    const dy = dummyNodes.alignedDY ?? dummyNodes.dy;
-    return (
-      <g>
-        {dummyNodes.nodes.map((dummyNode) => {
-          const node = graphData.nodes.get(dummyNode.id);
-          if (!node) {
-            return null;
-          }
-          const x = dummyNode.x + dx;
-          const y = dummyNode.y + dy;
-          const width = dummyNode.width + dWidth;
-          const height = dummyNode.height + dHeight;
-          const nodeConfig = getNodeConfig(node, graphConfig);
+export const AnimatingNodeGroup: React.FunctionComponent<
+  IAnimatingNodeGroup
+> = (props) => {
+  const { dummyNodes, graphData } = props;
+  const graphConfig = useGraphConfig();
+  const { dWidth, dHeight } = dummyNodes;
+  const dx = dummyNodes.alignedDX ?? dummyNodes.dx;
+  const dy = dummyNodes.alignedDY ?? dummyNodes.dy;
+  return (
+    <g>
+      {dummyNodes.nodes.map((dummyNode) => {
+        const node = graphData.nodes.get(dummyNode.id);
+        if (!node) {
+          return null;
+        }
+        const x = dummyNode.x + dx;
+        const y = dummyNode.y + dy;
+        const width = dummyNode.width + dWidth;
+        const height = dummyNode.height + dHeight;
+        const nodeConfig = getNodeConfig(node, graphConfig);
 
-          if (nodeConfig?.renderDummy) {
-            return nodeConfig.renderDummy({
-              ...node.inner,
-              x,
-              y,
-              width,
-              height,
-            });
-          }
-          return (
-            <Slots.NodeFrame
+        if (nodeConfig?.renderDummy) {
+          return nodeConfig.renderDummy({
+            ...node.inner,
+            x,
+            y,
+            width,
+            height,
+          });
+        }
+        return (
+          <Slots.NodeFrame
+            height={height}
+            width={width}
+            x={x}
+            y={y}
+            key={`node-frame-${dummyNode.id}`}
+          >
+            <rect
+              key={node.id}
+              transform={`translate(${x},${y})`}
               height={height}
               width={width}
-              x={x}
-              y={y}
-              key="node-frame"
-            >
-              <rect
-                key={node.id}
-                transform={`translate(${x},${y})`}
-                height={height}
-                width={width}
-                stroke={defaultColors.dummyNodeStroke}
-                strokeDasharray="4"
-                fill="none"
-              />
-            </Slots.NodeFrame>
-          );
-        })}
-      </g>
-    );
-  };
+              stroke={defaultColors.dummyNodeStroke}
+              strokeDasharray="4"
+              fill="none"
+            />
+          </Slots.NodeFrame>
+        );
+      })}
+    </g>
+  );
+};

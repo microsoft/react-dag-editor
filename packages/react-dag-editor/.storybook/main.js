04420a9cbc28c58518a3a414161f3047d8e62d79
intellild <intellild@outlook.com>
2023-03-27 15:38:25 +0800

    fix storybook

--- packages/react-dag-editor/.storybook/main.js
+++ packages/react-dag-editor/.storybook/main.js
@@ -4,11 +4,7 @@ module.exports = {
   ...rootMain,
 
   core: { ...rootMain.core, builder: "webpack5" },
-  stories: [
-    ...rootMain.stories,
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-essentials",
     ...rootMain.addons,

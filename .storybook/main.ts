8264f428be1b96b10a113129437f04412110dc9a
Jin Yucong <jinyucong911@outlook.com>
2024-05-27 11:41:59 +0800

    chore: Update Storybook addons and dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,18 +2,24 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@chromatic-com/storybook"
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {}
   },
-  docs: {
-    autodocs: "tag"
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
   }
 };
 export default config;

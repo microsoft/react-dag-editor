320a58fbebc94eb55f82a1cb56aad910cb7de28c
Yucong Jin <yucongj@microsoft.com>
2024-05-29 22:26:07 +0800

    Add Jest runner extension and Jest target to nx.json and tsconfig.json

---
+++ packages/react-dag-editor/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: "react-dag-editor",
+  preset: "../../jest.preset.js",
+  transform: {
+    "^.+\\.[tj]sx?$": ["babel-jest", { presets: ["@nrwl/react/babel"] }],
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../coverage/packages/react-dag-editor",
+  setupFiles: ["<rootDir>/src/test/unit/testSetup.js"],
+};

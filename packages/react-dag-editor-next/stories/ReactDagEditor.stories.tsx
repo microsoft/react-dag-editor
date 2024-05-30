109c994594d892fc8a090ed60ca6649541aad034
Jin Yucong <jinyucong911@outlook.com>
2024-05-30 14:49:46 +0800

    chore: Update tsconfig.storybook.json to include "jsx": "react-jsx"

---
+++ packages/react-dag-editor-next/stories/ReactDagEditor.stories.tsx
@@ -0,0 +1,9 @@
+export { ReactDagEditorStory } from "./components/ReactDagEditorStory";
+export { CanvasStory } from "./components/CanvasStory";
+export { ToolbarStory } from "./components/ToolbarStory";
+export { PaletteStory } from "./components/PaletteStory";
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  title: "React-Dag-Editor-Next",
+};
---
+++ packages/react-dag-editor-next/stories/components/CanvasStory.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import { Canvas } from "../../src";
+
+export const CanvasStory: FC = () => {
+  return <Canvas />;
+};

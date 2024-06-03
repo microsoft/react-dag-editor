4ddd2373d043d15379facf484482f0aa439da38b
Jin Yucong <jinyucong911@outlook.com>
2024-06-03 15:29:32 +0800

    chore: Update ReactDagEditor to accept viewModel prop

--- packages/react-dag-editor-next/.storybook/main.ts
+++ packages/react-dag-editor-next/.storybook/main.ts
@@ -1,15 +1,37 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
-  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
+  stories: ["../stories/**/*.@(mdx|stories.@(js|jsx|ts|tsx))"],
+  addons: [
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    "@chromatic-com/storybook",
+  ],
+  async viteFinal(viteConfig) {
+    return mergeConfig(viteConfig, {
+      plugins: [
+        tsconfigPaths({
+          projects: ["../../tsconfig.base.json"],
+        }),
+      ],
+    });
+  },
   framework: {
-    name: "@storybook/react-vite",
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {
       builder: {
         viteConfigPath: "vite.config.ts",
       },
     },
+  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
   },
 };
 
@@ -18,3 +40,7 @@ export default config;
 // To customize your Vite configuration you can use the viteFinal field.
 // Check https://storybook.js.org/docs/react/builders/vite#configuration
 // and https://nx.dev/recipes/storybook/custom-builder-configs
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
